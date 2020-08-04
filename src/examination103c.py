c075e4d6ede2c73953b67804dbd296988ee39440
UenoThinkStation <kamiya826@gmail.com>
2020-08-04 23:45:00 +0900

    Commit!

--- src/examination103c.py
+++ src/examination103c.py
@@ -17,8 +17,8 @@ def val_once(tap, neuron, epochs, train_list, test_list, result):
     X0 = np.array([])
     y0 = np.array([])
     for train_idx in train_list:
-        image = load_pickle('dataset/image' + str(train_idx).zfill(5) + '.pickle')
-        X0_tmp, y0_tmp = ml.data_shaping_with_overlapping(image.signal['x_0'], image.signal['x_2500'], 29, tap)
+        image = load_pickle('../data/input/train_0/train_0_' + str(train_idx).zfill(5) + '_10_8B10B.pickle')
+        X0_tmp, y0_tmp = ml.data_shaping_with_overlapping(image.signal['x_0'], image.signal['x_2500'], 501, tap)
         X0 = np.append(X0, X0_tmp).reshape(-1, tap * 2)
         y0 = np.append(y0, y0_tmp).reshape(-1, tap * 2)
     sc_y = StandardScaler()
@@ -27,8 +27,8 @@ def val_once(tap, neuron, epochs, train_list, test_list, result):
     X1 = np.array([])
     y1 = np.array([])
     for test_idx in test_list:
-        val_image = load_pickle('dataset/image' + str(test_idx).zfill(5) + '.pickle')
-        X1_tmp, y1_tmp = ml.data_shaping_with_overlapping(val_image.signal['x_0'], val_image.signal['x_2500'], 29, tap)
+        val_image = load_pickle('../data/input/train_0/train_0_' + str(test_idx).zfill(5) + '_10_8B10B.pickle')
+        X1_tmp, y1_tmp = ml.data_shaping_with_overlapping(val_image.signal['x_0'], val_image.signal['x_2500'], 501, tap)
         X1 = np.append(X1, X1_tmp).reshape(-1, tap * 2)
         y1 = np.append(y1, y1_tmp).reshape(-1, tap * 2)
     y1_std = sc_y.transform(y1)
@@ -63,8 +63,8 @@ def cross_val(tap, neuron, epochs, n_all, n_splits):
 
 
 def objective(trial):
-    neuron = trial.suggest_int('neuron', 10, 1000)
-    epochs = trial.suggest_int('epochs', 50, 1000)
+    neuron = trial.suggest_int('neuron', 10, 1500)
+    epochs = trial.suggest_int('epochs', 50, 1500)
     tap = 1
     n_all = 10
     n_splits = 5
@@ -77,11 +77,13 @@ if __name__ == '__main__':
     ml.log_off()
 
     # 最適化を実行
-    # study = optuna.create_study()
-    # study = load_pickle('../results/optuna007.pickle')
-    # study.optimize(objective, n_trials=5)
-
+    study = optuna.create_study()
+    # study = load_pickle('../results/optuna010.pickle')
+    study.optimize(objective, n_trials=20)
+    save_pickle(study, '../results/optuna010.pickle')
+    """
     tap = 1
     n_all = 10
     n_splits = 5
     score = cross_val(tap, 468, 488, n_all, n_splits)
+    """
