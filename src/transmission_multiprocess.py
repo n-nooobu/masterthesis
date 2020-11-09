e1badf2da689ed71ef8d859af7cf83226927a6b9
UenoThinkStation <kamiya826@gmail.com>
2020-11-09 23:26:22 +0900

    Commit!

--- src/transmission_multiprocess.py
+++ src/transmission_multiprocess.py
@@ -1,48 +1,93 @@
 import os
 import glob
-import cv2
 from multiprocessing import Process
 
-from pyopt.modulate import prbs, Modulate, eightb_tenb, image_to_binary
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+from pyopt import modulate as md
 from pyopt import transmission as tr
 from pyopt.util import save_pickle, load_pickle
 
 
-def image_transmission(process_index, target, space, code='8B10B', equalize=True):
-    """画像をbitに変換し長距離伝送シミュレーションを実行する"""
-    image_path = glob.glob(os.path.join('../image/' + target + '/', '*.jpg'))
-    image = cv2.imread(image_path[process_index])[::space, ::space].reshape(-1)
-    if code == '8B10B':
-        image_binary = eightb_tenb(image)
+def image_transmission(image_number):
+    target_dir = 'train_0'
+    step = 10  # =10 ---> (768, 1024) ---> (76, 102)
+    # image_number = 0
+    ebtb = True  # 8B10Bを行うか
+    form = 'RZ16QAM'  # 変調方式
+    n = 32  # 1シンボルあたりのサンプリング数[/symbol]
+    equalize = False  # 各シンボル数を均等にするか
+    baudrate = 28  # ボーレート[GBaud]
+    PdBm = 1  # 平均入力光パワー[dBm]
+    Ledfa = 100  # EDFAスパン[km]
+    stepedfa = 30  # SSFMの繰り返し計算ステップ数
+    gamma = 1.4  # 非線形係数[/W/km]
+    D = 16  # 分散パラメータ[ps/nm/km]
+    Alpha = 0.16  # 伝送損失[dB/km]
+    NF = 4  # ASE雑音指数[dB]
+    Lmax = 2500  # 伝送距離[km]
+    ase = True  # ASE雑音を考慮するか
+
+    df_dir = '../data/input/image.csv'
+    if os.path.exists(df_dir):
+        df = pd.read_csv(df_dir, index_col=0)
     else:
-        image_binary = image_to_binary(image)
+        cols = ['target_dir', 'step', 'image_number', 'ebtb',
+                    'form', 'n', 'equalize', 'baudrate', 'PdBm', 'Ledfa', 'stepedfa', 'gamma', 'D', 'Alpha', 'NF', 'Lmax', 'ase', 'data_path', 'image_path']
+        df = pd.DataFrame(index=[], columns=cols)
+        df.to_csv(df_dir)
 
-    if equalize:
-        mdl = Modulate('RZ16QAM', equalize=True)
+    condition = (df['target_dir']==target_dir) & (df['step']==step) & (df['image_number']==image_number) & (df['ebtb']==ebtb) & (df['form']==form) & \
+                         (df['n']==n) & (df['equalize']==equalize) & (df['baudrate']==baudrate) & (df['PdBm']==PdBm) & (df['Ledfa']==Ledfa) & \
+                         (df['stepedfa']==stepedfa) & (df['gamma']==gamma) & (df['D']==D) & (df['Alpha']==Alpha) & (df['NF']==NF) & (df['ase']==ase)
+
+    if sum(condition) > 0:
+        index = df[condition].index[0]
+        if df['Lmax'][index] >= Lmax:
+            print('指定された条件の伝送データはすでに存在します')
+        else:
+            data_path = df['data_path'][index]
+            sgnl = load_pickle(data_path)
+            sgnl.transmission(Lmax=Lmax, ase=ase)
+            save_pickle(sgnl, data_path)
+
+            df.loc[index, 'Lmax'] = Lmax
+            df.to_csv(df_dir)
     else:
-        mdl = Modulate('RZ16QAM', equalize=False)
-    sq = mdl.transform(image_binary)
+        image_dir = '../data/image/'
+        image_path_list = sorted(glob.glob(os.path.join(image_dir + target_dir, '*.jpg')))
+        image_path = image_path_list[image_number]
+        img = Image.open(image_path)
+        img_array = np.array(img)
+        imgsq = img_array[::step, ::step].reshape(-1)
+        if ebtb:
+            imgsq_binary = md.eightb_tenb(imgsq)
+        else:
+            imgsq_binary = md.image_to_binary(imgsq)
 
-    sgnl = tr.Signal(seq=sq, form='RZ16QAM', PdBm=1)
-    sgnl.transmission(Lmax=2500, ase=True)
+        mdl = md.Modulate(form=form, n=n, equalize=equalize)
+        modsq = mdl.transform(imgsq_binary)
+        sgnl = tr.Signal(seq=modsq, form=form, n=n, baudrate=baudrate, PdBm=PdBm, Ledfa=Ledfa, stepedfa=stepedfa,
+                                   gamma=gamma, D=D, Alpha=Alpha, NF=NF)
+        sgnl.transmission(Lmax=Lmax, ase=ase)
 
-    if code == '8B10B' and equalize:
-        path = '../data/input/' + target + '_8B10B_equalize/' + target + '_' + str(process_index).zfill(5) + '_' + str(space) + '.pickle'
-    elif code == '8B10B':
-        path = '../data/input/' + target + '_8B10B/' + target + '_' + str(process_index).zfill(5) + '_' + str(space) + '.pickle'
-    elif equalize:
-        path = '../data/input/' + target + '_equalize/' + target + '_' + str(process_index).zfill(5) + '_' + str(space) + '.pickle'
-    else:
-        path = '../data/input/' + target + '/' + target + '_' + str(process_index).zfill(5) + '_' + str(space) + '.pickle'
-    save_pickle(sgnl, path)
+        df = pd.read_csv(df_dir, index_col=0)
+        data_path = '../data/input/image/image_' + str(len(df)).zfill(10) + '.pickle'
+        save_pickle(sgnl, data_path)
+        sr = pd.Series([target_dir, step, image_number, ebtb,
+                                   form, n, equalize, baudrate, PdBm, Ledfa, stepedfa, gamma, D, Alpha, NF, Lmax, ase, data_path, image_path], index=df.columns)
+        df = df.append(sr, ignore_index=True)
+        df.to_csv(df_dir)
 
 
-def loop_multiprocessing(start, end, target='train', space=10, code='8B10B', equalize=False):
+def loop_multiprocessing(start, end):
     process_list = []
-    for i in range(start, end):
+    for image_number in range(start, end):
         process = Process(
             target=image_transmission,
-            kwargs={'process_index': i, 'target': target, 'space': space, 'code': code, 'equalize': equalize})
+            kwargs={'image_number': image_number})
         process.start()
         process_list.append(process)
     for process in process_list:
@@ -53,7 +98,4 @@ if __name__ == '__main__':
     for i in range(6):
         start = i * 25
         end = (i + 1) * 25
-        target = 'train_0'
-        space = 3
-        code = '8B10B'
-        loop_multiprocessing(start, end, target, space, code, equalize=True)
+        loop_multiprocessing(start, end)
